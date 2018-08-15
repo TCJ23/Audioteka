54a9175199f3b702ef54b2019ce006174b2d3e9f
TCJ23 <tcj2012@gmail.com>
2018-08-15 14:07:53 +0200

    solved by adam

--- src/Aadam_CUE.java
+++ src/Aadam_CUE.java
@@ -1,12 +1,26 @@
+import java.io.File;
+import java.io.FilenameFilter;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Aadam_CUE {
+
     public static void main(String[] args) throws Exception {
-        List<String> lines = Files.readAllLines(Paths.get("test.cue"));
+        File folder = new File("./");
+        File[] cue = folder.listFiles(findCUE());
+        List<String> source = new ArrayList<>(100);
+
+        for (File file : cue) {
+            System.out.println(file.getName());
+            source.add(file.getName());
+        }
+        Path path = Paths.get(source.get(0));
+
+        List<String> lines = Files.readAllLines(path);
         List<String> titles = new ArrayList<>();
         for (String line : lines) {
             if (line.contains("TITLE")) {
@@ -22,12 +36,26 @@ public class Aadam_CUE {
             if (line.contains("REM \"")) {
                 int start = line.indexOf("\"");
                 int end = line.lastIndexOf("\"");
-                result.add(line.substring(0, start + 1) + titles.get(count) + line.substring(end));
+                result.add(line.substring(0, start + 1) + titles.get(count) + ".mp3" + line.substring(end));
                 count++;
             } else {
                 result.add(line);
             }
         }
         result.forEach(System.out::println);
+        Files.write(path, result);
+    }
+
+
+    private static FilenameFilter findCUE() {
+        //        FilenameFilter filenameFilter = new FilenameFilter() {
+        FilenameFilter filenameFilter = (dir, name) -> {
+            if (name.endsWith(".cue")) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+        return filenameFilter;
     }
 }
